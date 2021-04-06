5278528d4bad3320463e33013b0d87aaeff75a24
Matthew Barton <mbarton@trinity.edu>
2021-04-06 12:31:37 -0500

    4/6 in class - database config

---
+++ server/app/util/CodeGen.scala
@@ -0,0 +1,11 @@
+package util
+
+object CodeGen extends App {
+  slick.codegen.SourceCodeGenerator.run(
+    "slick.jdbc.PostgresProfile", 
+    "org.postgresql.Driver",
+    "jdbc:postgresql://localhost/inclass",
+    "C:/Users/charg/WebApps/group-work-2-team-4-1/server/app", 
+    "models", None, None, true, false
+  )
+}
